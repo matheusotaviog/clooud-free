7eccd79883174b9c7eadb8bd06a3dc057d430f63
Matheus Otavio Gonçalves <matheusotaviog@gmail.com>
2026-07-11 19:27:39 -0300

    Retorno apenas 8 primeiros jogos em release, no html

--- js/main.js
+++ js/main.js
@@ -13,10 +13,29 @@ document.addEventListener('DOMContentLoaded',() => {
 
             const jogos = await resposta.json();
 
-            jogos.forEach(jogo => {
-                console.log(jogo.title);
-                console.log(jogo.short_description);
+            //Apenas 8 cards iniciais
+            const jogosEmDestaque = jogos.slice(0, 8);
+
+            let htmlJogos = '';
+
+            jogosEmDestaque.forEach(jogo => {
+                htmlJogos += `
+                    <div class="cartao-jogo">
+                        <div class="cartao-imagem">
+                            <img src="${jogo.thumbnail}" alt="Capa do jogo ${jogo.title}">
+                            <span class="tag tag-genero">${jogo.genre}</span>
+                            <span class="tag tag-free">FREE</span>
+                        </div>
+                        <div class="cartao-conteudo">
+                            <h3 class="cartao-titulo">${jogo.title}</h3>
+                            <p class="cartao-descricao">${jogo.short_description}</p>
+                            <a href="${jogo.game_url}" target="_blank" class="btn-jogar">JOGAR AGORA</a>
+                        </div>
+                    </div>
+                `;
             })
+
+            gridJogos.innerHTML = htmlJogos;
 
         } catch (erro) {
             console.log("Erro ao carregar os jogos:", erro)
